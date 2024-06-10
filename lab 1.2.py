1fc65c3bbd2ea8efbb2ec80c5bdcf04f3b43bd80
unknown <you@example.com>
2024-06-10 16:18:46 +0700

    first commit

---
+++ lab 1.2.py
@@ -0,0 +1,4 @@
+base = int(input("ฐาน: "))
+height = int(input("สูง: "))
+area = 1/2*base*height
+print("พื้นที่เท่ากับ: " + str(area))
---
+++ lab 1.3.py
@@ -0,0 +1,4 @@
+radius = int(input("รัศมี: "))
+area = 22/7*(radius*radius)
+print("พื้นที่เท่ากับ " + str(area))
+             
